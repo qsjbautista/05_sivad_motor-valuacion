6a33612d2ba9202bb6e0cf291b9f3d5ac5086dcb
osanchez <osanchez@d0c2015e-0c26-44a3-b760-eac3cb1bf761>
2016-11-03 22:54:44 +0000

    [sivad] * Se agregan carpetas para los microservicios de Siva Diamantes

---
+++ src/main/java/mx/com/nmp/ms/sivad/valuacion/MotorValuacionApplication.java
@@ -0,0 +1,12 @@
+package mx.com.nmp.ms.sivad.valuacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorValuacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorValuacionApplication.class, args);
+	}
+}
---
+++ src/test/java/mx/com/nmp/ms/sivad/valuacion/MotorValuacionApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.nmp.ms.sivad.valuacion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MotorValuacionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
